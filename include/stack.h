5b70c02aa38090d9c140c85f4e3eefffca622fd9
Python3x <biathloner2@mail.ru>
2019-11-10 19:03:47 +0300

    Modified stack.h and sample_stack.cpp

--- include/stack.h
+++ include/stack.h
@@ -3,8 +3,6 @@
 
 #include <iostream>
 
-using namespace std;
-
 #define MAX_STACK_SIZE 10000
 
 typedef unsigned int size_t;
@@ -13,81 +11,71 @@ template <typename ValType>
 class Stack
 {
 private:
-	template <typename ValType>
-	struct Node
-	{
-		ValType data;
-		Node<ValType> *next;
-	};
-
-	Node<ValType> *top;
+	ValType* mas;
 	size_t size;
+	size_t top_ind;
+	void Resize()
+	{
+		if (size == MAX_STACK_SIZE)
+			throw "Error: stack overflow";
+		size_t new_size = size + size / 3 + 1;
+		if (new_size > MAX_STACK_SIZE)
+			new_size = MAX_STACK_SIZE;
+		ValType* tmp = new ValType[new_size];
+		for (size_t i = 0; i < size; i++)
+			tmp[i] = mas[i];
+		delete[] mas;
+		mas = tmp;
+		size = new_size;
+	}
 
 public:
-	Stack() : top(nullptr), size(0) {}
+	Stack(size_t s = 10000)
+	{
+		if (size < 0)
+			throw "Error: illegal size";
+		size = s;
+		mas = new ValType[size];
+		top_ind = -1;
+	}
 
 	Stack(const Stack<ValType> &st)
 	{
 		size = st.size;
-		top = nullptr;
-		Node<ValType> *input_node = st.top;
-		Node<ValType> *cur_node, *prev_node = nullptr;
-		while (input_node != nullptr)
-		{
-			cur_node = new Node<ValType>;
-			cur_node->data = input_node->data;
-			cur_node->next = nullptr;
-
-			if (top == nullptr)
-			{
-				top = cur_node;
-				prev_node = cur_node;
-			}
-			else
-			{
-				prev_node->next = cur_node;
-				prev_node = prev_node->next;
-			}
-
-			input_node = input_node->next;
-		}
+		mas = new ValType[size];
+		for (size_t i = 0; i < size; i++)
+			mas[i] = st.mas[i];
+		top_ind = st.top_ind;
 	}
+
+	~Stack() { delete[] mas; }
+
+	bool Full() { return (top_ind + 1 == size); }
+	bool Empty() { return (top_ind == -1); }
+	size_t GetSize() { return top_ind + 1; }
 
 	void Push(ValType elem)
 	{
-		if (size == MAX_STACK_SIZE)
-			throw "Error: stack overflow";
-		Node<ValType> *tmp = new Node<ValType>;
-
-		tmp->data = elem;
-		tmp->next = top;
-		top = tmp;
-		size++;
+		if (Full())
+			Resize();
+		top_ind++;
+		mas[top_ind] = elem;
 	}
+
 	ValType Pop()
 	{
-		if (size == 0)
+		if (Empty())
 			throw "Error: stack underflow";
-
-		Node<ValType> *tmp;
-		ValType tmp_data = top->data;
-
-		tmp = top;
-		top = top->next;
-		delete tmp;
-		size--;
-
-		return tmp_data;
+		top_ind--;
+		return mas[top_ind + 1];
 	}
+
 	ValType Top()
 	{
-		if (size == 0)
+		if (Empty())
 			throw "Error: stack underflow";
-		return top->data;
+		return mas[top_ind];
 	}
-	size_t GetSize() { return size; }
-	bool Empty() { return (size == 0); }
-	bool Full() { return (size == MAX_STACK_SIZE); }
 };
 
 #endif // !_STACK_H_
--- samples/sample_stack.cpp
+++ samples/sample_stack.cpp
@@ -3,6 +3,8 @@
 #include "stack.h"
 #include <conio.h>
 
+using namespace std;
+
 int main()
 {
 	Stack<char> st;
